4278e73a275fca3841263d838467c8b711634e93
Omid Yousufi <omid_yousufi@hotmail.com>
2018-05-22 21:10:33 -0400

    Added psycopg2 package, and also added redis to SummaryView

--- mm_api/mm_api/views.py
+++ mm_api/mm_api/views.py
@@ -1,8 +1,14 @@
 from rest_framework.generics import GenericAPIView
 from django.http import HttpResponse
+from django.conf import settings
+import redis
 
 
 class SummaryView(GenericAPIView):
 
 	def get(self, request, *args, **kwargs):
-		return HttpResponse("SummaryView")
+		r = redis.StrictRedis(host=settings.REDIS_DATASTORE['host'],
+			port=settings.REDIS_DATASTORE['port'],
+			db=settings.REDIS_DATASTORE['db'])
+
+		return HttpResponse(r.get("test"))
